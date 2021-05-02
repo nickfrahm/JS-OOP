64a5e48eeb2ec3c3c0f7a68c1cff4f5df300a258
nickfrahm <nfrahm24@gmail.com>
2021-05-02 17:44:34 -0500

    styling for mobile first, need to practice

--- Book.js
+++ Book.js
@@ -1,9 +1,13 @@
-function Book(title, author, pages, read = false) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = () => {
-    return `${title} by ${author}, ${pages}, ${read ? "Read" : "Not read yet"}`;
-  };
+class Book {
+  constructor(title, author, pages, read = false) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = () => {
+      return `${title} by ${author}, ${pages}, ${
+        read ? "Read" : "Not read yet"
+      }`;
+    };
+  }
 }
